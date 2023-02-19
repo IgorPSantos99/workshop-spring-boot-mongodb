70a0132d6cf66928e693f6f6c5394988f8ef77e8
Igor Santos <igorpereira1999sp@gmail.com>
2023-02-19 19:54:26 -0300

    Endpoints to return User's Posts

--- src/main/java/com/igorsantos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/igorsantos/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.igorsantos.workshopmongo.domain.Post;
 import com.igorsantos.workshopmongo.domain.User;
 import com.igorsantos.workshopmongo.dto.UserDTO;
 import com.igorsantos.workshopmongo.services.UserService;
@@ -36,7 +37,7 @@ public class UserResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO>  findById(@PathVariable String id) {
-		User obj =service.findById(id);
+		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>>  findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
